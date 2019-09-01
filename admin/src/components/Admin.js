f19826a867b19101b24031663c32e16de08612f8
john ornelas <hosaka@johns-MacBook-Pro.local>
2019-09-01 00:09:12 -0400

    feat: build FE components for creating order

--- admin/src/components/Admin.js
+++ admin/src/components/Admin.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const styles = {
   // TODO use a better styling engine.
@@ -13,10 +15,22 @@ export class Admin extends Component {
     donuts: [],
     newName: '',
     newPrice: '',
+    //create order statE:
+    numFlavors: ['','',''],
+    quantities: {},
+    address: '',
+    tomorrow: new Date().setDate(new Date().getDate()+1),
+    // see orders:
+    today: new Date(),
   }
 
   componentDidMount() {
     this.fetchDonuts();
+  }
+
+  handleDateChange(date) {
+    console.log('date', date)
+    this.setState({ startDate: date });
   }
 
   fetchDonuts() {
@@ -65,9 +79,27 @@ export class Admin extends Component {
     this.setState({ [prop]: event.target.value });
   }
 
+  handleQuantity(name, e) {
+    // TODO: sanitize inputs to exclude alphabets
+    const quantities = { ...this.state.quantities };
+    quantities[name] = e.target.value;
+    this.setState({ quantities });
+  }
+
+  handleAddress(e) {
+    this.setState({ address: e.target.value });
+  }
+  handleOrder(e) {
+    e.preventDefault();
+
+    console.log(this.state.quantities);
+  }
+
   render() {
+    // TODO. use react router and build out different pages for each of the sections here:
     return (
       <div>
+        <h3>Current Donuts:</h3>
         {this.state.donuts.map(({ _id, name, price }) => {
           return (
             <div key={_id}>
@@ -77,7 +109,7 @@ export class Admin extends Component {
           )
         })}
         <p /><div>add new donut:</div>
-        <form onSubmit={(e) => this.handleAddDonut(e)}>
+        <form onSubmit={this.handleAddDonut.bind(this)}>
           <label>
             Name:
             <input type="text" value={this.state.newName} onChange={(e) => this.handleChange('newName', e)} />
@@ -88,6 +120,49 @@ export class Admin extends Component {
           </label>
           <input type="submit" value="Add donut" />
         </form>
+
+
+
+
+        <h3>create order:</h3>
+        <span>order date:</span>
+        <DatePicker
+          selected={this.state.tomorrow}
+          onChange={this.handleDateChange.bind(this)}
+          minDate={this.state.tomorrow}
+        />
+        {this.state.donuts.map(({ name, _id }) => (
+          <div key={name}>
+            <span>{name}</span>
+            <input
+              placeholder="leave empty for 0"
+              type="text"
+              value={this.state.quantities[name]}
+              onChange={(e) => this.handleQuantity(name, e)}
+            />
+          </div>
+        ))}
+        <input
+          type="text"
+          placeholder="address"
+          value={this.state.address}
+          onChange={(e) => this.handleAddress(e)}
+        />
+        <form onSubmit={this.handleOrder.bind(this)}>
+        <input
+          type="submit"
+          value="create order"
+        />
+        </form>
+
+
+
+
+      <h3>see orders:</h3>
+      <DatePicker
+        selected={this.state.today}
+        onChange={this.handleDateChange.bind(this)}
+      />
       </div>
     )
   }
