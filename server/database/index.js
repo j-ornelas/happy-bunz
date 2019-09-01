b5e073f7e7576e422dde553ba3301d528e7a2de4
john ornelas <hosaka@johns-MacBook-Pro.local>
2019-09-01 15:12:58 -0400

    feat: connect to atlas DB

--- server/database/index.js
+++ server/database/index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
-const dbName = 'happybunz';
 
-mongoose.connect(`mongodb://localhost/${dbName}`);
+const { ATLAS_USER, ATLAS_PASSWORD } = process.env;
+
+mongoose.connect(`mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@happybunzprod-es6el.mongodb.net/test?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('connected to database: ', dbName);
-});
+db.once('open', () => console.log('connected to database user: ', ATLAS_USER));
 
 module.exports = db;
