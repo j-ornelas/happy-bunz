5a0649970dab3be9c8859320fd9d9300bfe6daca
john ornelas <hosaka@johns-MacBook-Pro.local>
2019-08-31 20:56:54 -0400

    feat: send req. to delete donut from db on delete

--- admin/src/components/Admin.js
+++ admin/src/components/Admin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 const styles = {
+  // TODO use a better styling engine.
   deleteButton: {
     color: 'red',
     fontSize: 24,
@@ -19,13 +20,23 @@ export class Admin extends Component {
   }
 
   fetchDonuts() {
+    this.setState({ newName: '', newPrice: '' });
     fetch('/donuts/all')
       .then(res => res.json())
       .then(donuts => this.setState({ donuts }));
   }
 
-  handleRemove(index) {
-    console.log('removed!', index)
+  handleRemove(_id) {
+    fetch('/donuts/delete', {
+      method: 'delete',
+      body: JSON.stringify({ _id}),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.success) this.fetchDonuts()
+        if (data.message) alert(data.message);
+      })
   }
 
   handleAddDonut(e) {
@@ -57,11 +68,11 @@ export class Admin extends Component {
   render() {
     return (
       <div>
-        {this.state.donuts.map((donut, index) => {
+        {this.state.donuts.map(({ _id, name, price }) => {
           return (
-            <div key={donut.name + index}>
-            <span>{donut.name} - {donut.price}</span>
-            <span style={styles.deleteButton} onClick={() => this.handleRemove(index)}>{`     x`}</span>
+            <div key={_id}>
+            <span>{name} - {price}</span>
+            <span style={styles.deleteButton} onClick={() => this.handleRemove(_id)}>{`  x`}</span>
             </div>
           )
         })}
