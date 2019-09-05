d12c8abe86a8f8a629cdf7c0efb5ae4a18ddf904
john ornelas <johnnyoh1988@gmail.com>
2019-09-05 00:02:27 -0400

    feat: add temp coming-soon component

---
+++ admin/src/components/ComingSoon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ComingSoon extends Component {
+  render() {
+    return (
+      <div>
+        <div>Happy Bunz site coming soon....</div>
+        <div>(add '/admin' to the url to go to admin page)</div>
+      </div>
+    )
+  }
+}
+
+export default ComingSoon;
