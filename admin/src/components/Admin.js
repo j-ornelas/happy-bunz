cb2d0074b61cc9a79e70b6069ab47394bee2ea9b
john ornelas <hosaka@johns-MacBook-Pro.local>
2019-09-04 22:47:54 -0400

    feat: style header of page so user can see what page they're on

--- admin/src/components/Admin.js
+++ admin/src/components/Admin.js
@@ -20,28 +20,65 @@ export class Admin extends Component {
       .then(donuts => this.setState({ donuts }));
   }
 
+  isVisible(page) {
+    return page === this.state.visiblePage;
+  }
+
   render() {
-    const { visiblePage } = this.state;
     return (
-      <div>
-        <div>
-          <h3 onClick={() => this.setState({ visiblePage: 'UPDATE_DONUTS' })}>EDIT DONUTS</h3>
-          <h3 onClick={() => this.setState({ visiblePage: 'CREATE_ORDER' })}>ADD ORDERS</h3>
-          <h3 onClick={() => this.setState({ visiblePage: 'VIEW_ORDERS' })}>VIEW ORDERS</h3>
+      <div style={styles.body}>
+        <div style={styles.header}>
+        <span
+          style={this.isVisible('CREATE_ORDER') ? styles.highlightedHeaderItem : styles.headerItem}
+          onClick={() => this.setState({ visiblePage: 'CREATE_ORDER' })}>ADD ORDERS</span>
+          <span
+            style={this.isVisible('VIEW_ORDERS') ? styles.highlightedHeaderItem : styles.headerItem}
+            onClick={() => this.setState({ visiblePage: 'VIEW_ORDERS' })}>VIEW ORDERS</span>
+          <span
+            style={this.isVisible('UPDATE_DONUTS') ? styles.highlightedHeaderItem : styles.headerItem}
+            onClick={() => this.setState({ visiblePage: 'UPDATE_DONUTS' })}>EDIT DONUTS</span>
         </div>
-        {visiblePage === 'UPDATE_DONUTS' && (
+        {this.isVisible('CREATE_ORDER') && (
+          <CreateOrder donuts={this.state.donuts} />
+        )}
+        {this.isVisible('VIEW_ORDERS') && (
+          <ViewOrders />
+        )}
+        {this.isVisible('UPDATE_DONUTS') && (
           <UpdateDonuts
             donuts={this.state.donuts}
             fetchDonuts={this.fetchDonuts.bind(this)}
           />
         )}
-        {visiblePage === 'CREATE_ORDER' && (
-          <CreateOrder donuts={this.state.donuts} />
-        )}
-        {visiblePage === 'VIEW_ORDERS' && (
-          <ViewOrders />
-        )}
       </div>
     )
   }
+}
+
+const styles = {
+  body: {
+    padding: '10px'
+  },
+  header: {
+    display: 'flex',
+    width: '100vw',
+    height: '10vh',
+  },
+  headerItem: {
+    border: '1px solid lightgrey',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '33.3%',
+    height: '100%',
+  },
+  highlightedHeaderItem: {
+    border: '1px solid lightgrey',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgrey',
+    width: '33.3%',
+    height: '100%',
+  },
 }
