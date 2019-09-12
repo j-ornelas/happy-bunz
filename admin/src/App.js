a6eef6e83fc8ea1ac9b269e7eb2a988ffb8b104f
john ornelas <johnnyoh1988@gmail.com>
2019-09-12 01:13:47 -0400

    revert: lets avoid using specific /admin route for now until we figure out exactly how that'll work

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Admin } from './components/Admin';
-import ComingSoon from './components/ComingSoon';
+// import ComingSoon from './components/ComingSoon';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={ComingSoon} />
+      <Route exact path="/" component={Admin} />
       <Route path="/admin" component={Admin} />
     </Router>
   );
